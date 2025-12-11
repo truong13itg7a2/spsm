46fda5b3c2a1cbebab492308b152cb111efdab29
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-12-12 04:00:13 +0700

    035835121225

---
+++ JavaJ/modelspring/src/main/java/edu/txts/modelspring/ModelspringApplication.java
@@ -0,0 +1,13 @@
+package edu.txts.modelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelspringApplication.class, args);
+	}
+
+}
---
+++ JavaJ/modelspring/src/test/java/edu/txts/modelspring/ModelspringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.txts.modelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
